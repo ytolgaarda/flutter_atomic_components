fec96dbbf482bb481f9a8a0e7e40a32d9299e341
ytolgaarda <ty.5252@hotmail.com>
2025-03-03 22:51:50 +0300

    exporting

--- lib/atomic_components.dart
+++ lib/atomic_components.dart
@@ -1,7 +1,12 @@
 library;
 
+// Input Text Field
 export 'components/input/input.dart';
+// Chips
 export 'components/chip/chip.dart';
+// Spacing Widget
 export 'components/space/gap.dart';
-
+// Snackbar
 export 'components/snackbar/generic_snackbar.dart';
+// Buttons
+export 'components/buttons/shrink_button.dart';
